ecbc26a23a190eb011261a20e3f06ca8899fe43e
Haruaki <misbahulk2236@gmail.com>
2025-01-14 16:42:10 +0700

    move to list component

---
+++ src/lib/components/List/ListCard.svelte
@@ -0,0 +1,31 @@
+<script>
+	import ListDropdown from '$lib/components/List/ListDropdown.svelte';
+	import { Plus } from 'phosphor-svelte';
+
+	let { list } = $props();
+</script>
+
+<div class="card w-80 flex-shrink-0 bg-surface-800 p-3">
+	<div class="mb-4 flex items-center justify-between px-2 pt-1">
+		<p class="font-medium">{list.name}</p>
+		{#if !list.tasks}
+			<ListDropdown listId={list.id} />
+		{/if}
+	</div>
+
+	<div class="flex flex-col gap-3">
+		<!-- {#each (() => new Array(Math.ceil(Math.random() * 5)))() as _}
+						<div class="card w-full border border-surface-500 bg-surface-600 p-4 shadow">
+							<p class="font-medium">Hello</p>
+						</div>
+					{/each} -->
+	</div>
+
+	<button
+		class="btn mx-1 mb-1 text-surface-200 hover:text-white hover:preset-tonal"
+		class:mt-4={list.tasks?.length > 0}
+	>
+		<Plus size={20} />
+		Add a task
+	</button>
+</div>
--- src/routes/boards/[slug]/+page.svelte
+++ src/routes/boards/[slug]/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import ListCard from '../../../lib/components/List/ListCard.svelte';
+
 	import BoardPropertiesButton from '$lib/components/Board/BoardPropertiesButton.svelte';
 	import AddListButton from '$lib/components/List/AddListButton.svelte';
 	import ListDropdown from '$lib/components/List/ListDropdown.svelte';
@@ -18,30 +20,7 @@
 
 	<div class="mt-8 flex items-start gap-4 overflow-x-auto">
 		{#each boardState.board.lists as list (list.id)}
-			<div class="card w-80 flex-shrink-0 bg-surface-800 p-3">
-				<div class="mb-4 flex items-center justify-between px-2 pt-1">
-					<p class="font-medium">{list.name}</p>
-					{#if !list.tasks}
-						<ListDropdown listId={list.id} />
-					{/if}
-				</div>
-
-				<div class="flex flex-col gap-3">
-					<!-- {#each (() => new Array(Math.ceil(Math.random() * 5)))() as _}
-						<div class="card w-full border border-surface-500 bg-surface-600 p-4 shadow">
-							<p class="font-medium">Hello</p>
-						</div>
-					{/each} -->
-				</div>
-
-				<button
-					class="btn mx-1 mb-1 text-surface-200 hover:text-white hover:preset-tonal"
-					class:mt-4={list.tasks?.length > 0}
-				>
-					<Plus size={20} />
-					Add a task
-				</button>
-			</div>
+			<ListCard {list} />
 		{/each}
 
 		<AddListButton
